cff5f5b1eedcd6cec3ea51b1581b2f1f481a7b1a
Felix <jonfelixrico@gmail.com>
2022-10-10 00:49:08 +0800

    Remove API max timeouts

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,7 +1,7 @@
 import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { API_BASE_URL, API_MAX_TIMEOUTS } from 'src/env-vars.constants'
+import { API_BASE_URL } from 'src/env-vars.constants'
 import { PendingQuoteApiService } from './pending-quote-api/pending-quote-api.service'
 import { QuoteApiService } from './quote-api/quote-api.service'
 
@@ -11,7 +11,6 @@ import { QuoteApiService } from './quote-api/quote-api.service'
       imports: [ConfigModule],
       useFactory: async (cfg: ConfigService) => ({
         baseURL: cfg.getOrThrow<string>(API_BASE_URL),
-        timeout: cfg.get<number>(API_MAX_TIMEOUTS) ?? 5,
       }),
       inject: [ConfigService],
     }),
