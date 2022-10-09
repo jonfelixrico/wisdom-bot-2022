9e9a1b235553d175124cf7cd2159a9863d37fddb
Felix <jonfelixrico@gmail.com>
2022-10-10 00:54:09 +0800

    Integrate to handler

--- src/slash-commands/handlers/submit-handler/submit-handler.service.ts
+++ src/slash-commands/handlers/submit-handler/submit-handler.service.ts
@@ -1,14 +1,44 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common'
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common'
 import { ChatInputCommandInteraction } from 'discord.js'
+import { PendingQuoteApiService } from 'src/api/pending-quote-api/pending-quote-api.service'
 import { InteractionEventBus } from 'src/slash-commands/providers/interaction-event-bus/interaction-event-bus'
 
 @Injectable()
 export class SubmitHandlerService implements OnApplicationBootstrap {
-  constructor(private bus: InteractionEventBus) {}
+  private readonly LOGGER = new Logger(SubmitHandlerService.name)
 
-  private handle(interaction: ChatInputCommandInteraction) {
-    // TODO implement this
-    console.debug(interaction)
+  constructor(
+    private bus: InteractionEventBus,
+    private api: PendingQuoteApiService,
+  ) {}
+
+  private async handle(interaction: ChatInputCommandInteraction) {
+    try {
+      const { quoteId } = await this.api.submit({
+        authorId: interaction.options.getUser('author').id,
+        submitterId: interaction.user.id,
+        channelId: interaction.channelId,
+        serverId: interaction.guildId,
+        messageId: null,
+        content: interaction.options.getString('quote'),
+      })
+
+      interaction.reply({
+        ephemeral: true,
+        content: 'Ogey 👍',
+      })
+
+      this.LOGGER.log(
+        `Created quote ${quoteId} from interaction ${interaction.id}`,
+      )
+    } catch (e) {
+      this.LOGGER.error(e.message)
+      interaction.reply({
+        ephemeral: true,
+        content:
+          'Oops! Something went wrong while processing your submission. Try again later maybe?',
+      })
+    }
   }
 
   onApplicationBootstrap() {
