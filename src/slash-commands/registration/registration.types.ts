d8b5b45801c61419aae1b3d63f386a63eefc72b3
Felix <jonfelixrico@gmail.com>
2022-10-09 23:05:24 +0800

    Create service for registration

--- src/slash-commands/registration/registration.types.ts
+++ src/slash-commands/registration/registration.types.ts
@@ -3,13 +3,15 @@ import {
   SlashCommandSubcommandsOnlyBuilder,
 } from 'discord.js'
 
+export type CommandBuilder =
+  | SlashCommandBuilder
+  | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>
+  | SlashCommandSubcommandsOnlyBuilder
+
 export interface CommandToRegister {
   /**
    * Devland name of the command.
    */
   name: string
-  builder:
-    | SlashCommandBuilder
-    | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>
-    | SlashCommandSubcommandsOnlyBuilder
+  builder: CommandBuilder
 }
--- src/slash-commands/slash-commands.module.ts
+++ src/slash-commands/slash-commands.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common'
 import { SubmitHandlerService } from './handlers/submit-handler/submit-handler.service'
 import { ReceiveHandlerService } from './handlers/receive-handler/receive-handler.service'
 import { DiscordModule } from 'src/discord/discord.module'
+import { CommandRegistrationService } from './registration/command-registration/command-registration.service'
 
 @Module({
-  providers: [SubmitHandlerService, ReceiveHandlerService],
+  providers: [
+    SubmitHandlerService,
+    ReceiveHandlerService,
+    CommandRegistrationService,
+  ],
   imports: [DiscordModule],
 })
 export class SlashCommandsModule {}
