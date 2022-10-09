17cfbe1c8e86eb2dab0cf937d32d6ef7a8616058
Felix <jonfelixrico@gmail.com>
2022-10-10 02:03:12 +0800

    Integrate handler

--- src/slash-commands/handlers/receive-handler/receive-handler.service.ts
+++ src/slash-commands/handlers/receive-handler/receive-handler.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@nestjs/common'
 import { ChatInputCommandInteraction } from 'discord.js'
+import { QuoteApiService } from 'src/api/quote-api/quote-api.service'
 import { InteractionEventBus } from 'src/slash-commands/providers/interaction-event-bus/interaction-event-bus'
 
 @Injectable()
 export class ReceiveHandlerService {
-  constructor(private bus: InteractionEventBus) {}
+  constructor(private bus: InteractionEventBus, private api: QuoteApiService) {}
 
-  private handle(interaction: ChatInputCommandInteraction) {
-    // TODO implement this
-    console.debug(interaction)
+  private async handle(interaction: ChatInputCommandInteraction) {
+    const { guildId } = interaction
+    const randomQuote = await this.api.getRandomQuote(guildId)
+
+    if (!randomQuote) {
+      await interaction.reply({
+        ephemeral: true,
+        content: 'Sorry, but your Discord server has no approved quotes yet.',
+      })
+      return
+    }
+
+    // TODO implement a proper response
+    await interaction.reply(JSON.stringify(randomQuote))
+    await this.api.receive({
+      serverId: guildId,
+      channelId: interaction.channelId,
+      quoteId: randomQuote.id,
+      receiverId: interaction.user.id,
+    })
   }
 
   onApplicationBootstrap() {
