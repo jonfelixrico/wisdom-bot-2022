594b248c21b73d09e5441ad001b620f0f54e5a88
Felix <jonfelixrico@gmail.com>
2022-10-09 21:54:29 +0800

    Create services for the receive and submit commands

--- src/slash-commands/slash-commands.module.ts
+++ src/slash-commands/slash-commands.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common'
+import { SubmitHandlerService } from './handlers/submit-handler/submit-handler.service'
+import { ReceiveHandlerService } from './handlers/receive-handler/receive-handler.service'
 
-@Module({})
+@Module({
+  providers: [SubmitHandlerService, ReceiveHandlerService],
+})
 export class SlashCommandsModule {}
