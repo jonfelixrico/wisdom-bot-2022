dd904ed19294ebd484da353cec34b91539295453
Felix <jonfelixrico@gmail.com>
2022-10-09 22:30:36 +0800

    Import the discord module

--- src/slash-commands/slash-commands.module.ts
+++ src/slash-commands/slash-commands.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { SubmitHandlerService } from './handlers/submit-handler/submit-handler.service'
 import { ReceiveHandlerService } from './handlers/receive-handler/receive-handler.service'
+import { DiscordModule } from 'src/discord/discord.module'
 
 @Module({
   providers: [SubmitHandlerService, ReceiveHandlerService],
+  imports: [DiscordModule],
 })
 export class SlashCommandsModule {}
