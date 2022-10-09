ae2013165a685a57ef59409a0082e65653098de2
Felix <jonfelixrico@gmail.com>
2022-10-09 23:12:04 +0800

    Adjust the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config'
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.development.env',
+      envFilePath: ['.env', '.development.env'],
     }),
     SlashCommandsModule,
     DiscordModule,
