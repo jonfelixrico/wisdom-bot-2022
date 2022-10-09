7294656670583314fd83dda4c6e56ec29df2c8e8
Felix <jonfelixrico@gmail.com>
2022-10-10 00:09:44 +0800

    Integrate HttpModule

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,4 +1,18 @@
+import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { API_BASE_URL, API_MAX_TIMEOUTS } from 'src/env-vars.constants'
 
-@Module({})
+@Module({
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (cfg: ConfigService) => ({
+        baseURL: cfg.getOrThrow<string>(API_BASE_URL),
+        timeout: cfg.get<number>(API_MAX_TIMEOUTS) ?? 5,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class ApiModule {}
