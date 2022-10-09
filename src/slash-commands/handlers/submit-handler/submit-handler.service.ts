0efd197b6a3b3255cb4572a97612ef8d0b59b11b
Felix <jonfelixrico@gmail.com>
2022-10-09 23:41:03 +0800

    Create placeholder handlers for receive and submit

--- src/slash-commands/handlers/submit-handler/submit-handler.service.ts
+++ src/slash-commands/handlers/submit-handler/submit-handler.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common'
+import { ChatInputCommandInteraction } from 'discord.js'
+import { InteractionEventBus } from 'src/slash-commands/providers/interaction-event-bus/interaction-event-bus'
 
 @Injectable()
-export class SubmitHandlerService {}
+export class SubmitHandlerService implements OnApplicationBootstrap {
+  constructor(private bus: InteractionEventBus) {}
+
+  private handle(interaction: ChatInputCommandInteraction) {
+    // TODO implement this
+    console.debug(interaction)
+  }
+
+  onApplicationBootstrap() {
+    this.bus.subscribe((interaction) => {
+      if (!interaction.isChatInputCommand()) {
+        return
+      }
+
+      if (
+        interaction.commandName === 'submit' ||
+        (interaction.commandName === 'wisdom' &&
+          interaction.options.getSubcommand() === 'submit')
+      ) {
+        this.handle(interaction)
+      }
+    })
+  }
+}
