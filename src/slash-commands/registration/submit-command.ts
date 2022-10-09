b61c330897ca2ab8f2639202d4b9a27eb69fe2b4
Felix <jonfelixrico@gmail.com>
2022-10-09 22:05:52 +0800

    Create definition for the submit command

--- src/slash-commands/registration/submit-command.ts
+++ src/slash-commands/registration/submit-command.ts
@@ -1,5 +1,24 @@
 import { SlashCommandBuilder } from 'discord.js'
+import { CommandToRegister } from './types'
 
-export function buildSubmitCommand () {
-    return new SlashCommandBuilder().setName('submit')
+export function buildSubmitCommand(): CommandToRegister {
+  const builder = new SlashCommandBuilder()
+    .setName('submit')
+    .setDescription('Submit a quote')
+    .addStringOption((option) => {
+      return option
+        .setName('quote')
+        .setDescription('The quote to be added')
+        .setRequired(true)
+    })
+    .addUserOption((option) => {
+      return option
+        .setName('autor')
+        .setDescription('The user who said the quote')
+    })
+
+  return {
+    builder,
+    name: 'submit',
+  }
 }
