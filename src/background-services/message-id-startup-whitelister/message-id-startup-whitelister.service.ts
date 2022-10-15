b53181201a5bc2bce6ccfb0b4cd9c0a843d478f8
Felix <jonfelixrico@gmail.com>
2022-10-15 17:27:02 +0800

    Create the whitelister code

--- src/background-services/message-id-startup-whitelister/message-id-startup-whitelister.service.ts
+++ src/background-services/message-id-startup-whitelister/message-id-startup-whitelister.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common'
+import { Client } from 'discord.js'
+import { sprintf } from 'sprintf-js'
+import { PendingQuoteApiService } from 'src/api/pending-quote-api/pending-quote-api.service'
+import { MessageIdWhitelist } from 'src/discord/message-id-whitelist.abstract'
 
 @Injectable()
-export class MessageIdStartupWhitelisterService {}
+export class MessageIdStartupWhitelisterService
+  implements OnApplicationBootstrap
+{
+  private readonly LOGGER = new Logger(MessageIdStartupWhitelisterService.name)
+
+  constructor(
+    private api: PendingQuoteApiService,
+    private client: Client,
+    private whitelist: MessageIdWhitelist,
+  ) {}
+
+  async onApplicationBootstrap() {
+    const { LOGGER } = this
+
+    const guildIds = Array.from(this.client.guilds.cache.keys())
+    LOGGER.log(
+      sprintf(
+        'Whitelisting pending quote message ids for %d servers',
+        guildIds.length,
+      ),
+    )
+
+    for (const guildId of guildIds) {
+      LOGGER.debug(
+        sprintf('Retrieving pending quote message ids for server %s', guildId),
+      )
+
+      try {
+        const messageIds = await this.api.getPendingQuoteMessageIds(guildId)
+        for (const messageId of messageIds) {
+          await this.whitelist.add(messageId)
+        }
+
+        LOGGER.debug(
+          sprintf(
+            'Saved %d message ids for guild %s',
+            messageIds.length,
+            guildId,
+          ),
+        )
+      } catch (e) {
+        LOGGER.error(
+          sprintf(
+            'Encountered an error while getting pending quote message ids for server %s',
+            guildId,
+          ),
+          e,
+        )
+      }
+    }
+
+    LOGGER.log('Finished whitelisting pending quote message ids')
+  }
+}
