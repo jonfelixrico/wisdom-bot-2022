d9c4435106af10573a704c1d8603e799ca6ba329
Felix <jonfelixrico@gmail.com>
2022-10-15 16:08:35 +0800

    Add comment of intent

--- src/discord/message-id-whitelist.abstract.ts
+++ src/discord/message-id-whitelist.abstract.ts
@@ -1,3 +1,9 @@
+/**
+ * The intention behind this class is to allow the Discord-related services to know which messages to entertain,
+ * and which messages to ignore.
+ *
+ * This is part of the efforts to optimize performance.
+ */
 export abstract class MessageIdWhitelist {
   abstract add(messageId: string): Promise<void>
   abstract remove(messageId: string): Promise<void>
