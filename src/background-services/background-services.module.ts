22d6a200967e734b75966cae4d6ebd476c3042e4
Felix <jonfelixrico@gmail.com>
2022-10-15 17:21:14 +0800

    Add APIModule

--- src/background-services/background-services.module.ts
+++ src/background-services/background-services.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { DiscordModule } from 'src/discord/discord.module'
 import { QuoteApprovalWatcherService } from './quote-approval-watcher/quote-approval-watcher.service'
 import { MessageIdStartupWhitelisterService } from './message-id-startup-whitelister/message-id-startup-whitelister.service'
+import { ApiModule } from 'src/api/api.module'
 
 @Module({
   providers: [QuoteApprovalWatcherService, MessageIdStartupWhitelisterService],
-  imports: [DiscordModule],
+  imports: [DiscordModule, ApiModule],
 })
 export class BackgroundServicesModule {}
