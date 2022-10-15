c7dd36262182f748eb055be8344bbbb355cc330d
Felix <jonfelixrico@gmail.com>
2022-10-15 17:06:11 +0800

    Configure discord intents

--- src/discord/discord-client.provider.ts
+++ src/discord/discord-client.provider.ts
@@ -1,11 +1,23 @@
 import { Provider } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { Client, GatewayIntentBits } from 'discord.js'
+import { Client, GatewayIntentBits, Partials } from 'discord.js'
 import { DISCORD_TOKEN } from 'src/env-vars.constants'
 
 function clientFactory(cfg: ConfigService) {
   return new Promise((resolve, reject) => {
-    const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+    const client = new Client({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessages,
+      ],
+      partials: [
+        Partials.Message,
+        Partials.Channel,
+        Partials.Reaction,
+        Partials.User,
+      ],
+    })
 
     client.once('ready', () => {
       resolve(client)
