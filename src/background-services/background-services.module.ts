eabf90ed2d25107283581afee73061f86094cc11
Felix <jonfelixrico@gmail.com>
2022-10-15 15:25:48 +0800

    Import eventListenersModule

--- src/background-services/background-services.module.ts
+++ src/background-services/background-services.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { EventListenersModule } from 'src/event-listeners/event-listeners.module'
 import { QuoteApprovalWatcherService } from './quote-approval-watcher/quote-approval-watcher.service'
 
 @Module({
   providers: [QuoteApprovalWatcherService],
+  imports: [EventListenersModule],
 })
 export class BackgroundServicesModule {}
