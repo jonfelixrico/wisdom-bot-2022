5e857c92f7e624fc9b55b9ab85505ec16125d0ea
Felix <jonfelixrico@gmail.com>
2022-10-15 20:36:45 +0800

    Remove approval-dedicated service

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DiscordModule } from 'src/discord/discord.module'
-import { QuoteApprovePresentationService } from './quote-approve-presentation/quote-approve-presentation.service'
 import { QuoteSubmitPresentationService } from './quote-submit-presentation/quote-submit-presentation.service'
 import { UserAvatarService } from './user-avatar/user-avatar.service'
 
 @Module({
-  providers: [
-    QuoteApprovePresentationService,
-    QuoteSubmitPresentationService,
-    UserAvatarService,
-  ],
+  providers: [QuoteSubmitPresentationService, UserAvatarService],
   imports: [DiscordModule],
 })
 export class PresentationModule {}
