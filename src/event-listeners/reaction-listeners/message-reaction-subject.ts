6de6e95e4915285f659acaa72a356363a65d2afb
Felix <jonfelixrico@gmail.com>
2022-10-15 14:59:04 +0800

    Remove MessageReactionSubject

--- src/event-listeners/reaction-listeners/message-reaction-subject.ts
+++
@@ -1,8 +0,0 @@
-import { PartialMessageReaction } from 'discord.js'
-import { Subject } from 'rxjs'
-
-export class MessageReactionSubject extends Subject<PartialMessageReaction> {
-  /*
-   * `PartialMessageReaction` is just `MessageReaction` without the `count` property.
-   */
-}
