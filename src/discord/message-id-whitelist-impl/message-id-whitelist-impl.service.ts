1fab5247dc37520159c5fa7db316cbeae5d65931
Felix <jonfelixrico@gmail.com>
2022-10-15 16:05:29 +0800

    Implement the whitelist service

--- src/discord/message-id-whitelist-impl/message-id-whitelist-impl.service.ts
+++ src/discord/message-id-whitelist-impl/message-id-whitelist-impl.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { MessageIdWhitelist } from '../message-id-whitelist.abstract'
 
 @Injectable()
-export class MessageIdWhitelistImplService {}
+export class MessageIdWhitelistImplService extends MessageIdWhitelist {
+  private whitelist = new Set<string>()
+
+  async add(messageId: string): Promise<void> {
+    this.whitelist.add(messageId)
+  }
+
+  async remove(messageId: string): Promise<void> {
+    this.whitelist.delete(messageId)
+  }
+
+  async contains(messageId: string): Promise<boolean> {
+    return this.whitelist.has(messageId)
+  }
+}
