d16a69a0a2c528c793ea78d9fb23099cac6bf262
Felix <jonfelixrico@gmail.com>
2022-10-15 14:30:34 +0800

    Import the necessary modules

--- src/event-listeners/event-listeners.module.ts
+++ src/event-listeners/event-listeners.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ApiModule } from 'src/api/api.module'
+import { DiscordModule } from 'src/discord/discord.module'
 import { ReactionListenersService } from './reaction-listeners/reaction-listeners.service'
 
 @Module({
   providers: [ReactionListenersService],
+  imports: [DiscordModule, ApiModule],
 })
 export class EventListenersModule {}
