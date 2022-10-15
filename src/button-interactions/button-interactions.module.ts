7402f6328e85075e8fa3ba891a16a372cd3755f9
Felix <jonfelixrico@gmail.com>
2022-10-15 22:20:01 +0800

    Adjust the import

--- src/button-interactions/button-interactions.module.ts
+++ src/button-interactions/button-interactions.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { DiscordModule } from 'src/discord/discord.module'
 import { UpvoteHandlerService } from './handlers/upvote-handler/upvote-handler.service'
 
 @Module({
   providers: [UpvoteHandlerService],
+  imports: [DiscordModule],
 })
 export class ButtonInteractionsModule {}
