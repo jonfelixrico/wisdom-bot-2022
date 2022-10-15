26fa889f603aead8046789c33323e96cb7fe89e3
Felix <jonfelixrico@gmail.com>
2022-10-15 16:20:45 +0800

    Add logging to MessageIdWhitelistImplService

--- src/discord/message-id-whitelist-impl/message-id-whitelist-impl.service.ts
+++ src/discord/message-id-whitelist-impl/message-id-whitelist-impl.service.ts
@@ -1,16 +1,21 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
+import { sprintf } from 'sprintf-js'
 import { MessageIdWhitelist } from '../message-id-whitelist.abstract'
 
 @Injectable()
 export class MessageIdWhitelistImplService extends MessageIdWhitelist {
+  private readonly LOGGER = new Logger(MessageIdWhitelistImplService.name)
+
   private whitelist = new Set<string>()
 
   async add(messageId: string): Promise<void> {
     this.whitelist.add(messageId)
+    this.LOGGER.debug(sprintf('Added id %s', messageId))
   }
 
   async remove(messageId: string): Promise<void> {
     this.whitelist.delete(messageId)
+    this.LOGGER.debug(sprintf('Removed id %s', messageId))
   }
 
   async contains(messageId: string): Promise<boolean> {
