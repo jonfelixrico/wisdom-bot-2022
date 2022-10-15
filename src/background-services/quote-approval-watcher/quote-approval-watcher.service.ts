4d48389351b98f46b7bf0dc318ac63a190dda30d
Felix <jonfelixrico@gmail.com>
2022-10-15 15:51:09 +0800

    Push initial structure of QuoteApprovalWatcher

--- src/background-services/quote-approval-watcher/quote-approval-watcher.service.ts
+++ src/background-services/quote-approval-watcher/quote-approval-watcher.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common'
+import { PartialMessageReaction } from 'discord.js'
+import { ReactionChangesObservable } from 'src/event-listeners/reaction-changes-observable'
 
 @Injectable()
-export class QuoteApprovalWatcherService {}
+export class QuoteApprovalWatcherService implements OnApplicationBootstrap {
+  private readonly LOGGER = new Logger(QuoteApprovalWatcherService.name)
+
+  constructor(private obs: ReactionChangesObservable) {}
+
+  private handler(emit: PartialMessageReaction) {
+    // TODO implement this
+  }
+
+  onApplicationBootstrap() {
+    this.obs.subscribe(this.handler.bind(this))
+    this.LOGGER.log('Now watching for quote approvals...')
+  }
+}
