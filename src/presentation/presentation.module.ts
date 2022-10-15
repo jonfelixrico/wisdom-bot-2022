db6e5af88f3051395d092483de5dda9b520927ff
Felix <jonfelixrico@gmail.com>
2022-10-15 20:26:37 +0800

    Add DiscordModule as an import

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { DiscordModule } from 'src/discord/discord.module'
 import { QuoteApprovePresentationService } from './quote-approve-presentation/quote-approve-presentation.service'
 import { QuoteSubmitPresentationService } from './quote-submit-presentation/quote-submit-presentation.service'
 import { UserAvatarService } from './user-avatar/user-avatar.service'
@@ -9,5 +10,6 @@ import { UserAvatarService } from './user-avatar/user-avatar.service'
     QuoteSubmitPresentationService,
     UserAvatarService,
   ],
+  imports: [DiscordModule],
 })
 export class PresentationModule {}
