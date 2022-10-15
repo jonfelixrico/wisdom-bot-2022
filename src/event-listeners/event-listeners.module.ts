025d84d0845a7b223e83884c3e19da33a155fef9
Felix <jonfelixrico@gmail.com>
2022-10-15 15:08:05 +0800

    Remove unused imports

--- src/event-listeners/event-listeners.module.ts
+++ src/event-listeners/event-listeners.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common'
-import { ApiModule } from 'src/api/api.module'
 import { DiscordModule } from 'src/discord/discord.module'
 import { ReactionListenersService } from './reaction-listeners/reaction-listeners.service'
 
 @Module({
   providers: [ReactionListenersService],
-  imports: [DiscordModule, ApiModule],
+  imports: [DiscordModule],
 })
 export class EventListenersModule {}
