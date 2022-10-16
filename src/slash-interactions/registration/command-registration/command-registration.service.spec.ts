ec41d20a1c54d9e910df57db3341a9ee9ff18406
Felix <jonfelixrico@gmail.com>
2022-10-16 17:43:27 +0800

    Downgrade the original command-registration service

--- src/slash-interactions/registration/command-registration/command-registration.service.spec.ts
+++ src/slash-interactions/registration/command-registration/command-registration.service.spec.ts
@@ -1,15 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { CommandRegistrationService } from './command-registration.service'
+import { CommandRegistrationServiceObs } from './command-registration.service'
 
 describe('CommandRegistrationService', () => {
-  let service: CommandRegistrationService
+  let service: CommandRegistrationServiceObs
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommandRegistrationService],
+      providers: [CommandRegistrationServiceObs],
     }).compile()
 
-    service = module.get<CommandRegistrationService>(CommandRegistrationService)
+    service = module.get<CommandRegistrationServiceObs>(
+      CommandRegistrationServiceObs,
+    )
   })
 
   it('should be defined', () => {
