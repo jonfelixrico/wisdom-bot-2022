85e01d4a8c61dc5e8358bede89e85b88f9371bdc
Felix <jonfelixrico@gmail.com>
2022-10-16 17:53:48 +0800

    Apply minor fixes

--- src/slash-interactions/registration/command-registration/command-registration.service.ts
+++ src/slash-interactions/registration/command-registration/command-registration.service.ts
@@ -4,9 +4,9 @@ import { RECEIVE_COMMAND } from '../receive.command'
 import { SUBMIT_COMMAND } from '../submit.command'
 import { WISDOM_SUBCOMMANDS } from '../wisdom.subcommands'
 import { CommandRegistrationService } from 'src/discord/services/command-registration/command-registration.service'
-import { CommandToRegister } from '../registration.types'
+import { CommandBuilder } from 'src/discord/discord.types'
 
-const TO_REGISTER: CommandToRegister[] = [
+const TO_REGISTER: CommandBuilder[] = [
   RECEIVE_COMMAND,
   SUBMIT_COMMAND,
   ABOUT_COMMAND,
@@ -21,6 +21,6 @@ export class CommandRegistrationServiceObs implements OnApplicationBootstrap {
   constructor(private svc: CommandRegistrationService) {}
 
   async onApplicationBootstrap() {
-    this.svc.register(TO_REGISTER.map(({ builder }) => builder))
+    this.svc.register(TO_REGISTER)
   }
 }
