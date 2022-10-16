ca6dec77ccd421a38cf0a070a2888e9c319ac4fc
Felix <jonfelixrico@gmail.com>
2022-10-16 17:26:48 +0800

    Generate the command reg service

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { DISCORD_CLIENT_PROVIDER } from './providers/discord-client.provider'
+import { CommandRegistrationService } from './services/command-registration/command-registration.service';
 
 @Module({
-  providers: [DISCORD_CLIENT_PROVIDER],
+  providers: [DISCORD_CLIENT_PROVIDER, CommandRegistrationService],
   exports: [DISCORD_CLIENT_PROVIDER],
 })
 export class DiscordModule {}
