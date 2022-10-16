7df5ce4ce6ab5c70e6166561fc468ed1e2c5ab4a
Felix <jonfelixrico@gmail.com>
2022-10-16 17:27:37 +0800

    Run the format script

--- src/discord/services/command-registration/command-registration.service.spec.ts
+++ src/discord/services/command-registration/command-registration.service.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CommandRegistrationService } from './command-registration.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { CommandRegistrationService } from './command-registration.service'
 
 describe('CommandRegistrationService', () => {
-  let service: CommandRegistrationService;
+  let service: CommandRegistrationService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CommandRegistrationService],
-    }).compile();
+    }).compile()
 
-    service = module.get<CommandRegistrationService>(CommandRegistrationService);
-  });
+    service = module.get<CommandRegistrationService>(CommandRegistrationService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
