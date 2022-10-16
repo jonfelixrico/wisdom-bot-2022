a66cfa9ed6e157e3b1a9bc8c9f60d390ffac2d4f
Felix <jonfelixrico@gmail.com>
2022-10-16 20:16:42 +0800

    Create UserAvatarService

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { DISCORD_CLIENT_PROVIDER } from './providers/discord-client.provider'
+import { UserAvatarService } from './services/user-avatar/user-avatar.service'
 
 @Module({
-  providers: [DISCORD_CLIENT_PROVIDER],
-  exports: [DISCORD_CLIENT_PROVIDER],
+  providers: [DISCORD_CLIENT_PROVIDER, UserAvatarService],
+  exports: [DISCORD_CLIENT_PROVIDER, UserAvatarService],
 })
 export class DiscordModule {}
