037f6bbe0485a59364124cf21331b97c30df1499
Felix <jonfelixrico@gmail.com>
2022-12-02 02:50:31 +0800

    Integrate nest winston

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core'
+import { WinstonModule, utilities } from 'nest-winston'
+import * as winston from 'winston'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            utilities.format.nestLike('MyApp', {
+              colors: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  })
   await app.listen(3000)
 }
 bootstrap()
