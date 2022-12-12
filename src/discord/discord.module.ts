e09c6e21d61762c9e69e9e51e000e8dca4a4c4a2
Felix <jonfelixrico@gmail.com>
2022-12-12 12:51:44 +0800

    Issue #22: Log interactions (#23)
    * Generate the service
    
    * Implement initial logger
    
    * Add debug logging to the logger
    
    * Make logging use verbose instead of debug
    
    * Remove spaces
    
    * Add quotes

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common'
 import { DISCORD_CLIENT_PROVIDER } from './providers/discord-client.provider'
 import { UserAvatarService } from './services/user-avatar/user-avatar.service'
 import { MessageService } from './services/message/message.service'
+import { InteractionLoggerService } from './interaction-logger/interaction-logger.service'
 
 @Module({
-  providers: [DISCORD_CLIENT_PROVIDER, UserAvatarService, MessageService],
+  providers: [
+    DISCORD_CLIENT_PROVIDER,
+    UserAvatarService,
+    MessageService,
+    InteractionLoggerService,
+  ],
   exports: [DISCORD_CLIENT_PROVIDER, UserAvatarService, MessageService],
 })
 export class DiscordModule {}
