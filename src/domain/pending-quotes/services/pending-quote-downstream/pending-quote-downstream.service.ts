3ec7f8503bb6669f2e8954c004bad964d5a02895
Felix <jonfelixrico@gmail.com>
2022-12-12 11:12:27 +0800

    Directly move processApproval into PendingQuoteDownstreamService

--- src/domain/pending-quotes/services/pending-quote-downstream/pending-quote-downstream.service.ts
+++ src/domain/pending-quotes/services/pending-quote-downstream/pending-quote-downstream.service.ts
@@ -3,7 +3,6 @@ import { concatMap, debounceTime, from, groupBy, mergeMap, Subject } from 'rxjs'
 import { GetPendingQuoteRespDto } from 'src/api/pending-quote-api/dto/get-pending-quote-dto.interface'
 import { PendingQuoteApiService } from 'src/api/pending-quote-api/pending-quote-api.service'
 import { MessageService } from 'src/discord/services/message/message.service'
-import { PendingQuoteApprovalService } from '../../pending-quote-approval/pending-quote-approval.service'
 import { PendingQuoteExpirationService } from '../../pending-quote-expiration/pending-quote-expiration.service'
 import { PendingQuoteMessageGeneratorService } from '../pending-quote-message-generator/pending-quote-message-generator.service'
 
@@ -17,7 +16,6 @@ export class PendingQuoteDownstreamService {
     private api: PendingQuoteApiService,
     private msgGen: PendingQuoteMessageGeneratorService,
     private msgSvc: MessageService,
-    private approveSvc: PendingQuoteApprovalService,
     private expireSvc: PendingQuoteExpirationService,
   ) {
     this.initListener()
@@ -51,6 +49,39 @@ export class PendingQuoteDownstreamService {
     }
   }
 
+  async processApproval(quote: GetPendingQuoteRespDto) {
+    const { LOGGER } = this
+    LOGGER.debug(`Handling the approval of quote ${quote.id}`)
+
+    try {
+      await this.api.finalizeStatus({
+        quoteId: quote.id,
+        status: 'APPROVED',
+      })
+      LOGGER.debug(`Finalized status of quote ${quote.id} as approved`)
+
+      const message = await this.msgSvc.getMessage(quote)
+      await message.edit(
+        await this.msgGen.generateForApproval({
+          ...quote,
+          year: new Date(quote.submitDt).getFullYear(),
+        }),
+      )
+
+      await message.channel.send({
+        reply: {
+          messageReference: message,
+        },
+        content: 'This quote has been approved 🎊🎉',
+      })
+    } catch (e) {
+      LOGGER.error(
+        `Error encountered while processing the approval of quote ${quote.id}`,
+        e,
+      )
+    }
+  }
+
   private async handle(quoteId: string) {
     const quoteData = await this.api.get({ quoteId })
     if (!quoteData) {
@@ -64,7 +95,7 @@ export class PendingQuoteDownstreamService {
       // check if quote has reached enough numbers of upvotes
       Object.values(quoteData.votes ?? {}).length >= quoteData.requiredVoteCount
     ) {
-      await this.approveSvc.processApproval(quoteData)
+      await this.processApproval(quoteData)
     } else {
       await this.reRenderOngoing(quoteData)
     }
