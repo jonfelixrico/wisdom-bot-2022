a521e310318629e547cd3e1883b95350e6ba2b65
Felix <jonfelixrico@gmail.com>
2023-03-28 19:05:30 +0800

    Adhoc #63: Create alpha on develop-* merge and use different versioning (#66)
    * Listen to develop-* instead of alpha-*
    
    * Remove SNAPSHOT tag
    
    * Create action.yml file
    
    * Use the action
    
    * Make the action more flexible
    
    * Apply the tweaks
    
    * Adjust the dockerfile to include BUILD_VERSION env
    
    * Integrate the use of BUILD_VERSION env arg
    
    * Add logging in the pipeline

--- src/domain/system/services/app-info/app-info.service.ts
+++ src/domain/system/services/app-info/app-info.service.ts
@@ -1,12 +1,22 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { version } from 'package.json'
+import { BUILD_VERSION } from 'src/env-vars.constants'
 
 @Injectable()
-export class AppInfoService {
+export class AppInfoService implements OnApplicationBootstrap {
+  private readonly LOGGER = new Logger(AppInfoService.name)
+
+  constructor(private cfg: ConfigService) {}
+
   /**
    * Get the version of this bot.
    */
   get version(): string {
-    return version
+    return this.cfg.get(BUILD_VERSION) || version
+  }
+
+  onApplicationBootstrap() {
+    this.LOGGER.log(`Running on version ${this.version}`)
   }
 }
